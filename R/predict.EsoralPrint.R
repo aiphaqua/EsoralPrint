bea29a8c5786c3e47f09af290d0f3b5ec8dda759
Mark Jai <mark.jai@yahoo.com>
2026-02-14 14:28:58 -0500

    major update

--- R/predict.EsoralPrint.R
+++ R/predict.EsoralPrint.R
@@ -5,22 +5,54 @@
 #' @param cutoff numeric threshold for classification
 #' @param ... Optional additional arguments (currently unused)
 #'
-#' @return Data.frame with LinearScore, MalignancyProbability, and optionally Class.
+#' @return Data.frame with Likelihood (the degree to which a sample is likely to be malignant) and, optionally, Malignant (Yes or No).
 #' @export
-predict.EsoralPrint <- function(object, newdata, type = c("prob","class"), cutoff=0.5, ...) {
+predict.EsoralPrint <- function(object, newdata,
+                                type = c("prob","class"),
+                                cutoff = 0.5, ...) {
+
   type <- match.arg(type)
-  Z <- rep(object$intercept, ncol(newdata))
+
+  if (is.null(rownames(newdata)))
+    stop("Row names must be gene symbols.")
+
+  required_genes <- unique(c(object$isoPairs$gene_h,
+                             object$isoPairs$gene_k))
+
+  missing_genes <- setdiff(required_genes, rownames(newdata))
+  if (length(missing_genes) > 0)
+    stop(paste("Missing genes:",
+               paste(missing_genes, collapse = ", ")))
+
+  Z <- rep(as.numeric(object$intercept), ncol(newdata))
+
   for (i in seq_len(nrow(object$isoPairs))) {
+
     g1 <- object$isoPairs$gene_h[i]
     g2 <- object$isoPairs$gene_k[i]
     beta <- object$isoPairs$beta[i]
-    x1 <- newdata[g1, ]
-    x2 <- newdata[g2, ]
-    iso_value <- ifelse(x1 > x2,1,ifelse(x1 < x2,-1,0))
+
+    x1 <- as.numeric(newdata[g1, , drop = TRUE])
+    x2 <- as.numeric(newdata[g2, , drop = TRUE])
+
+    iso_value <- ifelse(x1 > x2, 1,
+                        ifelse(x1 < x2, -1, 0))
+
     Z <- Z + beta * iso_value
   }
-  MP <- 1/(1+exp(-Z))
-  results <- data.frame(Sample=colnames(newdata), LinearScore=Z, MalignancyProbability=MP)
-  if (type=="class") results$Class <- ifelse(MP>=cutoff,"Malignant","Benign")
+
+  MP <- 1 - 1 / (1 + exp(-Z))
+
+  results <- data.frame(
+    Sample = colnames(newdata),
+    Likelihood = MP,
+    row.names = NULL
+  )
+
+  if (type == "class")
+    results$Malignant <- ifelse(MP >= cutoff,
+                                "Yes",
+                                "No")
+
   return(results)
 }
